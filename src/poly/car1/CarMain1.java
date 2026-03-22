aac23f43bb69bfb3aabee76bee7b324d775c599b
thbins <joons0218@gmail.com>
2026-03-22 20:52:45 +0900

    학습: 다형성과 OCP 응용 연습 문제

--- src/poly/car1/CarMain1.java
+++ src/poly/car1/CarMain1.java
@@ -15,5 +15,10 @@ public class CarMain1 {
         Car model3Car = new Model3Car();
         driver.setCar(model3Car); // Driver의 car 변수의 참조값을 K3에서 Model3로 변경
         driver.drive();
+
+        // Model3 -> NewCar로 변경
+        NewCar newCar = new NewCar();
+        driver.setCar(newCar);
+        driver.drive();
     }
 }
